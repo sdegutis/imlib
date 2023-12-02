67e959a4b2845b13e4d6d1cc0b6ec2f5d31d95fd
Steven Degutis <sbdegutis@gmail.com>
2023-12-02 12:10:33 -0600

    Preparing to turn into a library.

--- site/core/generated.ts
+++
@@ -1,9 +0,0 @@
-import { outfiles } from './outfiles.js';
-
-export function generated(name: string, generateContent: () => string) {
-  const fullpath = `/generated/${name}`;
-  if (!outfiles.has(fullpath)) {
-    outfiles.set(fullpath, generateContent());
-  }
-  return fullpath;
-}
--- site/scripts/data/snippet-ids.json.tsx
+++
@@ -1,5 +0,0 @@
-import { allSnippets } from '../../model/snippets.js';
-
-export default <>
-  {JSON.stringify(allSnippets.map(snippet => snippet.slug))}
-</>;
